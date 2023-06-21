7f6d3afb4da68e41310043815e25e03c3de495fd
waihtunsoe <waitunsoe.dev@gmail.com>
2023-06-21 13:49:28 +0630

    travel table & model

---
+++ app/Models/Travel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Travel extends Model
+{
+    use HasFactory, Sluggable;
+
+    protected $table = 'travels';
+
+    protected $fillable = [
+        'is_public',
+        'slug',
+        'name',
+        'description',
+        'number_of_days'
+    ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function numberOfNights(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) => $attributes['number_of_days'] - 1
+        );
+    }
+
+    // public function getNumberOfNightsAttribute()
+    // {
+    //     return $this->number_of_days - 1;
+    // }
+}
